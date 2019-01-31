833120b11acb1dfad50b6417a3543abdb580a831
Wallace Oliveira <wallacesales@gmail.com>
2019-01-31 14:45:26 -0300

    Primeiros programas.

---
+++ JavaBasicoLoiane/src/aula06/Argumentos.java
@@ -0,0 +1,17 @@
+package aula06;
+
+class Argumentos {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo! " + meuNome());
+
+	}
+	
+	public static String meuNome() {
+		
+		return "Meu segundo programa em Java.";
+		
+	}
+
+}
